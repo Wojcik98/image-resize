e69b238d65e828483d7e99f17956a3201ce7dc7c
Wojcik <wojcikmichal98@gmail.com>
2017-06-04 20:27:51 +0200

    Add script

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+from PIL import Image
+
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('width')
+    arg_parser.add_argument('height')
+    arg_parser.add_argument('image_path', nargs='+')
+    args = arg_parser.parse_args()
+
+    return args.width, args.height, args.image_path
+
+
+if __name__ == '__main__':
+    max_width, max_height, paths = parse_args()
+
+    for path in paths:
+        file = open(path, 'rb')
+        img = Image.open(file)
+
+        width, height = img.size
+        ratio = min(float(max_width)/width, float(max_height)/height)
+        new_width = int(ratio * width)
+        new_height = int(ratio * height)
+
+        img = img.resize((new_width, new_height))
+
+        filename, extension = os.path.splitext(path)
+        new_path = filename + '_resized' + extension
+        img.save(new_path)
+
+        file.close()
